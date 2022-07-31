f63c34be76eac2cb61ae70245ff6ac8b7f368ce2
Ahmad Alsaady <97070340+alsaady20@users.noreply.github.com>
2022-07-31 17:56:56 -0400

    9th commit

--- newsletter/settings.py
+++ newsletter/settings.py
@@ -122,7 +122,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
-STATIC_ROOT = [BASE_DIR / 'staticfiles']
+STATIC_ROOT = "C:\Users\alsaa\L1 Newsletter\newsletter\staticfiles"
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [BASE_DIR / 'static']
 
