7976431e2f43c3f54f9e2e887ea57b3df0a8960a
Ahmad Alsaady <97070340+alsaady20@users.noreply.github.com>
2022-08-03 13:19:41 -0400

    17th commit

--- newsletter/settings.py
+++ newsletter/settings.py
@@ -24,9 +24,9 @@ STATIC_DIR = Path(BASE_DIR) / 'static'
 SECRET_KEY = 'django-insecure-118vfljd#_pv)k)kjtj$u&2u*e_%h1gk$z7y=lpded77pcien='
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ['lube1romul.us','lube1express.herokuapp.com','127.0.0.1']
+ALLOWED_HOSTS = ['www.lube1romul.us','lube1express.herokuapp.com','127.0.0.1']
 
 
 # Application definition
